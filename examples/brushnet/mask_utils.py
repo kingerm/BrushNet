cea73ac24096b44e3db5f58e6fefdc1f0c4eeb30
Wang Qiwen <78541951+kingerm@users.noreply.github.com>
2024-07-31 14:37:25 +0800

    采用gauss_blur后

--- examples/brushnet/mask_utils.py
+++ examples/brushnet/mask_utils.py
@@ -10,6 +10,7 @@ from transformers import CLIPProcessor, CLIPModel
 from functools import reduce
 from time import time
 
+
 def draw_mask_to_image(image, mask, alpha=0.5):
     if isinstance(image, Image.Image):
         image = np.array(image)
@@ -17,13 +18,14 @@ def draw_mask_to_image(image, mask, alpha=0.5):
     overlay = np.zeros_like(image, dtype=np.uint8)
 
     for c in range(0, 3):
-        overlay[:, :, c] = (mask_rgba[:, :, c] * (mask_rgba[:, :, 3] / 255.0) 
-                            +  image[:, :, c] * (1.0 - mask_rgba[:, :, 3] / 255.0))
+        overlay[:, :, c] = (mask_rgba[:, :, c] * (mask_rgba[:, :, 3] / 255.0)
+                            + image[:, :, c] * (1.0 - mask_rgba[:, :, 3] / 255.0))
     result = cv2.addWeighted(image, 1, overlay, alpha, 0)
 
-    x,y = cal_max_mask_barycentre(mask)
+    x, y = cal_max_mask_barycentre(mask)
     cv2.circle(result, (x, y), radius=5, color=(0, 0, 255), thickness=-1)
     return result
+
 
 def extract_connected_regions(mask):
     if mask.shape[-1] == 3:
@@ -36,13 +38,14 @@ def extract_connected_regions(mask):
         region_mask = np.zeros_like(mask)
         cv2.drawContours(region_mask, [contour], -1, 255, thickness=cv2.FILLED)
         region_image = cv2.bitwise_and(mask, mask, mask=region_mask)
-        if np.sum(region_image>128) < 16*16:
+        if np.sum(region_image > 128) < 16 * 16:
             continue
 
         connected_region_images.append(region_image)
-        connected_region_area.append(np.sum(region_image==255))
+        connected_region_area.append(np.sum(region_image == 255))
         full_mask = cv2.bitwise_or(full_mask, region_mask)
     return connected_region_images, full_mask, connected_region_area
+
 
 def cal_max_mask_barycentre(mask):
     labeled_mask, num_features = label(mask)
@@ -55,6 +58,7 @@ def cal_max_mask_barycentre(mask):
 
     print("Max connected component centroid: ({:.2f}, {:.2f})".format(center_x, center_y))
     return center_x, center_y
+
 
 def cal_mask_barycentre(mask, min_area=256):
     labeled_mask, num_features = label(mask)
@@ -69,6 +73,7 @@ def cal_mask_barycentre(mask, min_area=256):
 
     return np.array(center_coordinates)
 
+
 def get_finemask(mask, sty_image, predictor):
     if isinstance(mask, str):
         mask = np.array(Image.open(mask))
@@ -78,7 +83,7 @@ def get_finemask(mask, sty_image, predictor):
         pass
     else:
         raise ValueError("Not compatible format for rough mask")
-    
+
     if isinstance(sty_image, str):
         np_img = np.array(Image.open(sty_image))
     elif isinstance(sty_image, Image.Image):
@@ -87,26 +92,28 @@ def get_finemask(mask, sty_image, predictor):
         pass
     else:
         raise ValueError("Not compatible format for style image")
-    
-    coords = cal_mask_barycentre(mask) 
+
+    coords = cal_mask_barycentre(mask)
     print(f'Found {len(coords)} connected regions, centers by {coords}.')
-    
+
     predictor = SamPredictor(predictor)
     predictor.set_image(np_img)
-    masks, _, _ = predictor.predict(point_coords = coords,point_labels=np.array([1]*len(coords)))
-    masks = np.transpose(masks, (1,2,0)).astype(np.uint8)
+    masks, _, _ = predictor.predict(point_coords=coords, point_labels=np.array([1] * len(coords)))
+    masks = np.transpose(masks, (1, 2, 0)).astype(np.uint8)
     bimasks = cv2.cvtColor(masks, cv2.COLOR_RGB2GRAY)
-    bimasks[bimasks>0]=255
-    np_img[bimasks==0] = 0
+    bimasks[bimasks > 0] = 255
+    np_img[bimasks == 0] = 0
     masked_out_pil = Image.fromarray(np_img)
-    
+
     return np_img, bimasks
 
-def dilate_mask(binary_mask, kernel_size = 3, iterations= 10):
+
+def dilate_mask(binary_mask, kernel_size=3, iterations=10):
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size, kernel_size))
     dilated_mask = cv2.dilate(binary_mask, kernel, iterations=iterations)
     dilate_diff = cv2.absdiff(dilated_mask, binary_mask)
     return dilated_mask, dilate_diff
+
 
 def clip_mask_filter(prompts, image, mask):
     model_id = "/path/to/clip-vit-large-patch14"
@@ -119,23 +126,24 @@ def clip_mask_filter(prompts, image, mask):
     max_idx = connected_region_area.index(max(connected_region_area))
     clip_score = []
     for region in connected_regions:
-        print(np.sum(region==255))
-        region_image = np.expand_dims(region, axis=2)*image
+        print(np.sum(region == 255))
+        region_image = np.expand_dims(region, axis=2) * image
         text = prompts
         inputs = processor(text, images=region_image, return_tensors="pt")
         outputs = model(**inputs)
-        logits_per_image = outputs.logits_per_image  
+        logits_per_image = outputs.logits_per_image
         clip_score.append(logits_per_image)
-    
+
     retain_idxs = []
     for i in range(len(clip_score)):
         if clip_score[i] >= clip_score[max_idx] - 0.5:
             retain_idxs.append(i)
     retain_regions = [connected_regions[j] for j in retain_idxs]
-    final_mask = reduce(lambda x,y:x+y, retain_regions)
-    
+    final_mask = reduce(lambda x, y: x + y, retain_regions)
+
     cv2.imwrite('fm.png', final_mask)
     return final_mask
+
 
 def filter_small_regions(mask, region_thres=64):
     if len(mask.shape) == 3:
@@ -144,10 +152,11 @@ def filter_small_regions(mask, region_thres=64):
     regions = find_objects(labeled_image)
     for i, region in enumerate(regions):
         if region is not None:
-            area = measurements.sum(labeled_image[region], labeled_image[region], index=i+1)
+            area = measurements.sum(labeled_image[region], labeled_image[region], index=i + 1)
             if area < region_thres:
                 mask[region] = 0
     return mask
+
 
 def get_finemask_everything(rough_mask, style_image, predictor):
     if isinstance(style_image, Image.Image):
@@ -157,15 +166,16 @@ def get_finemask_everything(rough_mask, style_image, predictor):
     t1 = time()
     segmentations = mask_generator.generate(style_image)
     t2 = time()
-    print(t2-t1)
+    print(t2 - t1)
     rough_mask = filter_small_regions(rough_mask)
     fine_mask = find_most_overlapping_segmentation(rough_mask, segmentations)
     fine_mask = filter_small_regions(fine_mask)
-    fine_mask[fine_mask>0]=255
-    style_image[fine_mask==0] = 0
+    fine_mask[fine_mask > 0] = 255
+    style_image[fine_mask == 0] = 0
     masked_out_pil = Image.fromarray(style_image)
 
     return style_image, fine_mask
+
 
 def find_most_overlapping_segmentation(mask, segmentations):
     if len(segmentations) == 0:
@@ -174,7 +184,7 @@ def find_most_overlapping_segmentation(mask, segmentations):
         mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
     if mask.max() > 1:
         mask = (mask / mask.max()).astype(np.uint8)
-    
+
     max_intersection_area = 0
     max_iou = 0
     seg_ratio = 0
@@ -189,20 +199,20 @@ def find_most_overlapping_segmentation(mask, segmentations):
         iou = intersection_area / union_area
         if iou > max_iou:
             max_iou = iou
-            seg_ratio = intersection_area/np.sum(segmentation.astype(np.uint8))
+            seg_ratio = intersection_area / np.sum(segmentation.astype(np.uint8))
             best_segmentation = segmentation
 
     print(f'Best Segmentation IOU: {max_iou:.2f}, seg_ratio:{seg_ratio:.2f}')
     best_segmentation = best_segmentation.astype(np.uint8) * 255
     return best_segmentation
 
+
 def transfer_color(src, target):
-    
     src_b, src_g, src_r = cv2.split(src)
     tgt_b, tgt_g, tgt_r = cv2.split(target)
 
     (tgt_b_mean, tgt_b_std) = cv2.meanStdDev(tgt_b)
-    (tgt_g_mean, tgt_g_std) = cv2.meanStdDev(tgt_g) 
+    (tgt_g_mean, tgt_g_std) = cv2.meanStdDev(tgt_g)
     (tgt_r_mean, tgt_r_std) = cv2.meanStdDev(tgt_r)
 
     src_b = (src_b - tgt_b_mean) / tgt_b_std
@@ -213,7 +223,7 @@ def transfer_color(src, target):
     trans_g = src_g * tgt_g_std + tgt_g_mean
     trans_r = src_r * tgt_r_std + tgt_r_mean
 
-    trans = cv2.merge([trans_b, trans_g, trans_r])  
+    trans = cv2.merge([trans_b, trans_g, trans_r])
     return trans
 
 
@@ -221,13 +231,14 @@ def fill_holes(im):
     if im.shape[-1] == 3:
         im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     contours, _ = cv2.findContours(im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    
+
     new_img = np.zeros_like(img).astype(np.uint8)
     img_template = np.expand_dims(new_img, axis=-1)
-    img_template = np.tile(img_template, (1,1,3))
+    img_template = np.tile(img_template, (1, 1, 3))
     img1 = np.zeros_like(img_template).astype(np.uint8)
-    im = cv2.drawContours(img1, contours, -1, (255,255,255), cv2.FILLED)
+    im = cv2.drawContours(img1, contours, -1, (255, 255, 255), cv2.FILLED)
     return im
+
 
 def color_transition(image, mask, radius):
     if isinstance(image, Image.Image):
@@ -237,7 +248,7 @@ def color_transition(image, mask, radius):
     alpha[mask == 255] = 1.0
     for y in range(radius, image.shape[0] - radius):
         for x in range(radius, image.shape[1] - radius):
-            if mask[y, x] == 255:  
+            if mask[y, x] == 255:
                 surrounding_colors = image[y - radius:y + radius + 1, x - radius:x + radius + 1]
                 original_color = image[y, x]
                 new_color = np.mean(surrounding_colors, axis=(0, 1))
@@ -247,13 +258,13 @@ def color_transition(image, mask, radius):
 
     return result, alpha
 
+
 def subtract_fft_images(fg_dilated_mask, bg_dilated_mask, threshold=35, high_freq_thres=200):
-    
     if fg_dilated_mask.shape[-1] == 3:
         fg_dilated_mask = cv2.cvtColor(fg_dilated_mask, cv2.COLOR_RGB2GRAY)
     if bg_dilated_mask.shape[-1] == 3:
         bg_dilated_mask = cv2.cvtColor(bg_dilated_mask, cv2.COLOR_RGB2GRAY)
-    
+
     f1 = np.fft.fft2(fg_dilated_mask)
     f2 = np.fft.fft2(bg_dilated_mask)
     f1 = np.fft.fftshift(f1)
@@ -265,7 +276,7 @@ def subtract_fft_images(fg_dilated_mask, bg_dilated_mask, threshold=35, high_fre
     mask = np.zeros((rows, cols), np.uint8)
     center = (crow, ccol)
     cv2.circle(mask, center, radius, 1, -1)
-    f1 = f1 * mask
+    f1 = f1 * mask  # 不要低通滤波器
     f2 = f2 * mask
 
     f = f1 - f2
@@ -276,21 +287,22 @@ def subtract_fft_images(fg_dilated_mask, bg_dilated_mask, threshold=35, high_fre
     img = img.astype(np.uint8)
     img[img > threshold] = 255
     img[img <= threshold] = 0
-
-    kernel = np.ones((21,21),np.uint8)
+    cv2.imwrite('fft_xiangjian.png', img)
+    kernel = np.ones((21, 21), np.uint8)  # 增大闭运算的kernel size，可以优化边缘连接效果。闭运算可以填补小黑洞
     img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    cv2.imwrite('morph_close_img.png', img)
     # contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    cv_version = cv2.__version__.split('.')[0] # check cv2 version, to avoid unpack error
+    cv_version = cv2.__version__.split('.')[0]  # check cv2 version, to avoid unpack error
 
     if int(cv_version) >= 4:
         contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     else:
         _, contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    
+
     img1 = np.zeros_like(img).astype(np.uint8)
-    img1 = np.expand_dims(img1, axis=-1)  
-    img1 = np.tile(img1, (1, 1, 3))  
-    img = cv2.drawContours(img1, contours, -1, (255,255,255), cv2.FILLED)
+    img1 = np.expand_dims(img1, axis=-1)
+    img1 = np.tile(img1, (1, 1, 3))
+    img = cv2.drawContours(img1, contours, -1, (255, 255, 255), cv2.FILLED)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     return img
 
--- examples/brushnet/test_brushnet.py
+++ examples/brushnet/test_brushnet.py
@@ -12,18 +12,23 @@ from migc.migc_pipeline import StableDiffusionMIGCPipeline, MIGCProcessor, Atten
 from migc.migc_utils import load_migc
 from torchvision.ops import masks_to_boxes
 import math
+# 下面是zone相关代码
+import argparse
+from mask_utils import *
+from PIL import ImageEnhance
 
-def read_mask(mask_path, kernel):
+def read_mask(mask_path, kernel, iterations):
     name = mask_path[4: -4]
+    mask = cv2.imread(mask_path)
     mask_image = 1.*(cv2.imread(mask_path).sum(-1)>255)[:,:,np.newaxis]
-    dilated_mask_image = cv2.dilate(mask_image, kernel, iterations=1)[..., np.newaxis]
+    dilated_mask_image = cv2.dilate(mask_image, kernel, iterations)[..., np.newaxis]
     mask_image_t = torch.from_numpy(mask_image).permute(2, 0, 1)
     box_xyxy = masks_to_boxes(mask_image_t)
     h, w, _ = mask_image.shape
     box_xyxy = box_xyxy.squeeze(0)
     box_xyxy[0], box_xyxy[2] = box_xyxy[0] / w, box_xyxy[2] / w
     box_xyxy[1], box_xyxy[3] = box_xyxy[1] / h, box_xyxy[3] / h
-    return mask_image, box_xyxy.numpy().tolist(), name, dilated_mask_image
+    return mask_image, box_xyxy.numpy().tolist(), name, dilated_mask_image, mask
 def cal_hw(h, w):
     ref_aspect_ratio = h / w  # 定义参考的高宽比，目的是使变形后的size的高宽比尽可能接近原来的高宽比
     # 考虑到这样一个事实：对于正整数a而言，其相邻的两个32的倍数必然是一个除64余0， 一个除64余32.
@@ -61,11 +66,84 @@ def cal_hw(h, w):
                 h_new = h_new2
     return int(h_new), int(w_new)
 
+def zone_ops(style_image, original_image, mask, args, sam_predictor):
+    """
 
+    Args:
+        style_image: sd output image
+        original_image: resized image or edited zone_ops image
+        mask: input mask
+        args: parser's args
+
+    Returns: edited zone_ops image
+
+    """
+
+    mask = cv2.resize(mask, (w, h), interpolation=cv2.INTER_CUBIC)  # 对输入的mask进行resize
+    masked_out, mask = get_finemask_everything(mask, style_image,
+                                               sam_predictor)  # masked_out是被mask后的style_image，mask是优化后的Mask
+    # 这里不反色，会变成蓝色的
+    # masked_out = cv2.cvtColor(masked_out, cv2.COLOR_BGR2RGB)
+    # cv2.imwrite('masked_out1.png', masked_out)
+    mask_image = mask
+
+    # dilated_mask_all, _ = dilate_mask(mask_image, iterations=args.dilate_strength)  # 确实需要dilate，因为物体不一定能完全和想要的区域吻合
+    # 7. ops based on mode
+    # style_image_np = np.array(style_image)
+    style_image_np = cv2.cvtColor(np.array(style_image), cv2.COLOR_BGR2RGB)
+    gt_image_np = original_image  # original_image由于是直接imread的，所以不用反色
+    # gt_image_np = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+    # canvas = gt_image_np.copy()
+    #
+    # canvas[mask_image > 0] = masked_out[dilated_mask_all > 0]  # 这里可以把mask换成dilate_mask_all
+    mask_blurred = cv2.GaussianBlur(mask_image, (11, 11), 0)
+    mask_blurred = mask_blurred[..., np.newaxis] / 255
+    new_canvas = gt_image_np * (1 - mask_blurred) + style_image_np * mask_blurred
+    # cv2.imwrite('new_canvas.png', new_canvas)
+    # edge_bimask = dilated_mask_all - mask_image
+    # kernel = np.ones((3, 3), np.uint8)  # 增大闭运算的kernel size，可以优化边缘连接效果。闭运算可以填补小黑洞
+    # edge_bimask = cv2.morphologyEx(edge_bimask, cv2.MORPH_OPEN, kernel)
+    # cv2.imwrite('edge_bimask.png', edge_bimask)
+    # edge_bimask_blurred = cv2.GaussianBlur(edge_bimask, (5, 5), 0)
+    # cv2.imwrite('edge_bimask_blurred.png', edge_bimask)
+    # edge_bimask_blurred = edge_bimask_blurred[..., np.newaxis] / 255
+    # new_canvas = new_canvas * (1 - edge_bimask_blurred) + gt_image_np * edge_bimask_blurred
+    # new_canvas = gt_image_np.copy()
+    # new_canvas[mask_image > 0] = style_image_np[mask_image > 0]
+    # new_canvas[edge_bimask > 0] = style_image_np[edge_bimask > 0] * args.alpha + gt_image_np[edge_bimask > 0] * (
+    #         1 - args.alpha)
+    # image_np = np.array(image)
+    # init_image_np = cv2.imread(image_path)[:, :, ::-1]
+    # mask_np = 1. * (cv2.imread(mask_path).sum(-1) > 255)[:, :, np.newaxis]
+    #
+    # # blur, you can adjust the parameters for better performance
+    # mask_blurred = cv2.GaussianBlur(mask_np * 255, (21, 21), 0) / 255
+    # mask_blurred = mask_blurred[:, :, np.newaxis]
+    # mask_np = 1 - (1 - mask_np) * (1 - mask_blurred)
+    #
+    # image_pasted = init_image_np * (1 - mask_np) + image_np * mask_np
+    # image_pasted = image_pasted.astype(image_np.dtype)
+    # image = Image.fromarray(image_pasted)
+    return new_canvas
+def zone_parse_args():
+    parser = argparse.ArgumentParser(description='Process some parameters.')
+    parser.add_argument('--sam_ckpt_path', type=str, default="./ckpts/sam_vit_h_4b8939.pth",
+                        help='The path to SAM checkpoint')
+    parser.add_argument('--threshold', type=int, default=20, help='The threshold of edge smoother')
+    parser.add_argument('--alpha', type=float, default=0.3,
+                        help='The alpha value for blending style image and original image')
+    parser.add_argument('--blend_beta', type=float, default=0.2, help='The beta value for fusing IP2P and MB')
+    parser.add_argument('--image_guidance_scale', type=float, default=7.5, help='The image guidance scale')
+    parser.add_argument('--dilate_strength', type=int, default=1, help='The dilate strength')
+    parser.add_argument('--brightness', type=float, default=1, help='The brightness of the style image')
+
+    args = parser.parse_args()
+    return args
+args = zone_parse_args()
+sam_predictor = sam_model_registry["vit_h"](checkpoint=args.sam_ckpt_path,).to('cuda')  # 初始化sam
 # choose the base model here
 base_model_path = "data/ckpt/realisticVisionV60B1_v51VAE"
 # base_model_path = "runwayml/stable-diffusion-v1-5"
-# base_model_path = "Uminosachi/realisticVisionV51_v51VAE-inpainting"
 # base_model_path = "CompVis/stable-diffusion-v1-4"  # 因为migc用的是sd1.4，和sd1.5的unet其实并不相同！
 # brushnet用sd1.4也能跑起来。试试migc能不能用sd1.5跑起来？=>答案是可以的。所以都用sd1.5跑吧
 # input brushnet ckpt path
@@ -115,40 +193,60 @@ mask_path3 = 'src/renlian_scarf.jpg'
 mask_path4 = 'src/renlian_medal.jpg'
 # mask_path3 = 'src/renlian3_necklace.jpg'
 # mask_path2 = 'src/sofa_mask2.jpg'
-kernel = np.ones((0, 0), np.uint8)  # dilate核
-mask_image1, box_xyxy1, name1, d_mask_image1 = read_mask(mask_path1, kernel)
-mask_image2, box_xyxy2, name2, d_mask_image2 = read_mask(mask_path2, kernel)
-mask_image3, box_xyxy3, name3, d_mask_image3 = read_mask(mask_path3, kernel)
-mask_image4, box_xyxy4, name4, d_mask_image4 = read_mask(mask_path4, kernel)
+dilate_kernel = np.ones((3, 3), np.uint8)  # dilate核
+dilate_iterations = 2
+mask_image1, box_xyxy1, name1, d_mask_image1, mask_zone1 = read_mask(mask_path1, dilate_kernel, dilate_iterations)
+mask_image2, box_xyxy2, name2, d_mask_image2, mask_zone2 = read_mask(mask_path2, dilate_kernel, dilate_iterations)
+mask_image3, box_xyxy3, name3, d_mask_image3, mask_zone3 = read_mask(mask_path3, dilate_kernel, dilate_iterations)
+mask_image4, box_xyxy4, name4, d_mask_image4, mask_zone4 = read_mask(mask_path4, dilate_kernel, dilate_iterations)
+# cv2.imwrite('renlian_resize_mask1.png', mask_zone1)
+# cv2.imwrite('renlian_resize_mask2.png', mask_zone2)
+# cv2.imwrite('renlian_resize_mask3.png', mask_zone3)
+# cv2.imwrite('renlian_resize_mask4.png', mask_zone4)
 # 定义膨胀kernel  # 上面的mask_image都是(512, 512, 1)，所以给dilated_mask_image添加一维之后就可以直接相加了
 mask_image_wo_d = mask_image1 + mask_image2 + mask_image3 + mask_image4# + mask_image3 + mask_image4  # 记录没有dilate的mask
-mask_image = mask_image1 + mask_image2 + mask_image3 + mask_image4# + d_mask_image3 + d_mask_image4
+mask_image = d_mask_image1 + d_mask_image2 + d_mask_image3 + d_mask_image4# + d_mask_image3 + d_mask_image4
 mask_image[mask_image > 1.0] = 1.0  # 若mask有重叠，重叠区域相加会大于1，要把它们置为1
 mask = mask_image  # 这里把mask_image给保存下来，方便后面画图
 mask_wo_d = mask_image_wo_d
 name_t = name1 + name2 + name3 + name4
 name = 'output' + name_t + '.png'
-
+###获取原图的h w###
+h, w, _ = init_image.shape
+h, w = cal_hw(h, w)
+#################
 init_image_c = init_image
 mask_image_c = np.zeros_like(mask_image)  # mask_image中不需要编辑的区域对应为黑色
+resize_image = cv2.resize(init_image_c, (w, h), interpolation=cv2.INTER_CUBIC)  # cv2.resize的img应为ndarray
+resize_image = Image.fromarray(resize_image.astype(np.uint8)).convert("RGB")
+resize_image.save('resize_renlian.png')
 init_image_c = Image.fromarray(init_image_c.astype(np.uint8)).convert("RGB")
 mask_image_c = Image.fromarray(mask_image_c.astype(np.uint8).repeat(3,-1)*255).convert("RGB")
 init_image = init_image * (1-mask_image)
 # init_image = init_image * mask_image
-h, w, _ = init_image.shape
-h, w = cal_hw(h, w)
+
+
 init_image = Image.fromarray(init_image.astype(np.uint8)).convert("RGB")
+
 mask_image = Image.fromarray(mask_image.astype(np.uint8).repeat(3,-1)*255).convert("RGB")  # 最右边一维重复三遍，转黑白图再转RGB
 # init_image.save('/home/xkzhu/yhx/BrushNet/examples/brushnet/init_image_T.png', quality=100)
 # mask_image.save('/home/xkzhu/yhx/BrushNet/examples/brushnet/mask_renlian_seg.png', quality=100)
-generator = torch.Generator("cuda").manual_seed(1234)
 seed = 1234
 seed_everything(seed)
+original_image = cv2.imread('resize_renlian.png')
+
+
+generator = torch.Generator("cuda").manual_seed(1234)
 # 初始化migc需要的形参  # 需要把brushnet的prompt相关代码全部换成migc的
 prompt_final = [['masterpiece, best quality, brown colored hat, black colored glasses, gray colored scarf, yellow colored medal',
                  'brown colored hat', 'black colored glasses', 'gray colored scarf', 'yellow colored medal'
                  ]]
 bboxes = [[box_xyxy1, box_xyxy2, box_xyxy3, box_xyxy4]]  # 优化了代码，使其不再需要手动计算bboxes，更加智能！
+########暂时注释掉，尝试zone的效果如何##########
+# prompt_final = [['masterpiece, best quality, brown colored hat',
+#                  'brown colored hat'
+#                  ]]
+# bboxes = [[box_xyxy1]]  # 优化了代码，使其不再需要手动计算bboxes，更加智能！
 negative_prompt = 'worst quality, low quality, bad anatomy, watermark, text, blurry'
 
 image = pipe(
@@ -182,7 +280,16 @@ if blended:
     image_pasted=init_image_np * (1-mask_np) + image_np*mask_np
     image_pasted=image_pasted.astype(image_np.dtype)
     image=Image.fromarray(image_pasted)
-
+enhancer = ImageEnhance.Brightness(image)  # 使用图像增强看看效果
+image = enhancer.enhance(args.brightness)
+img1 = zone_ops(image, original_image, mask_zone1, args, sam_predictor)
+cv2.imwrite('zone_multi_output1.png', img1)
+img2 = zone_ops(image, img1, mask_zone2, args, sam_predictor)
+cv2.imwrite('zone_multi_output2.png', img2)
+img3 = zone_ops(image, img2, mask_zone3, args, sam_predictor)
+cv2.imwrite('zone_multi_output3.png', img3)
+img4 = zone_ops(image, img3, mask_zone4, args, sam_predictor)
+cv2.imwrite('zone_multi_output4.png', img4)
 image.save(name)
 image = pipe.draw_box_desc(image, bboxes[0], prompt_final[0][1:])
 mask_img = pipe.draw_mask(image, mask)
